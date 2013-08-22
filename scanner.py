eef56cae206daac6e7de950c9eb80d8623be4406
Matthew Iannucci <mpiannucci@gmail.com>
2013-08-22 13:15:23 -0400

    added color module. Changed comments and some strings.

--- scanner.py
+++ scanner.py
@@ -4,7 +4,9 @@ import shutil
 import sys
 import transmissionrpc
 import ConfigParser
+
 from TextMessage import SendText
+from pyColor import color
 
 configPath = '/home/pi/Media-Scanner/scanner.cfg'
 config = ConfigParser.RawConfigParser()
@@ -43,69 +45,51 @@ mustypes = ["*.mp3"]
 # Empty list for the matches to get copied too
 matches = []
 
-# Make some cute command line colors
-class bcolor:
-    GREEN = '\033[92m'
-    BLUE = '\033[94m'
-    RED = '\033[91m'
-    ENDC = '\033[0m'
-    
-    def disable(self):
-        self.BLUE = ''
-        self.GREEN = ''
-        self.RED = ''
-        self.ENDC = ''
-
-"""
-Iterate through possible file types to find potential movie files
-"""
 def MovieScan():
+    """ Scan and mvoe movie files """
     for root, dirnames, filenames in os.walk(dlPath):
         for extend in movtypes:
             for filename in fnmatch.filter(filenames, extend):
                 matches.append(os.path.join(root, filename))
                 print(os.path.join(root, filename))
                 shutil.move(os.path.join(root, filename), os.path.join(moviePath, filename))
-                print bcolor.GREEN + 'File succesfully moved!' + bcolor.ENDC
+                print color.GREEN + 'File succesfully moved!' + color.ENDC
     print "Finished Scanning For Movies"
 
-"""
-Iterate through the TV show dictionary to mvoe tv show episodes into respective folders
-"""
 def TvScan():
+    """ Scan and move Tv Shows """
     for root, dirnames, filenames in os.walk(dlPath):
         for key, location in tvshows.iteritems():
             for filename in fnmatch.filter(filenames, key):
                 matches.append(os.path.join(root, filename))
                 print(os.path.join(root, filename))
                 shutil.move(os.path.join(root, filename), os.path.join(location, filename))
-                print bcolor.GREEN + 'File succesfully moved!' + bcolor.ENDC
+                print color.GREEN + 'File succesfully moved!' + color.ENDC
     print "Finished Scanning For TV Shows"
 
 def MusicScan():
+    """ Scan and move music """
     for root, dirnames, filenames in os.walk(dlPath):
         for extend in mustypes:
             for filename in fnmatch.filter(filenames, extend):
                 matches.append(os.path.join(root, filename))
                 print(os.path.join(root, filename))
                 shutil.move(os.path.join(root, filename), os.path.join(musicPath, filename))
-                print bcolor.GREEN + 'File succesfully moved!' + bcolor.ENDC
+                print color.GREEN + 'File succesfully moved!' + color.ENDC
     print "Finished Scanning For Music"
 
-"""
-Smart Scan scans tv shows then movies to weed out tv shows from movie searches in one step
-"""
 def ScanAll():
     MusicScan()
     TvScan()
     MovieScan()
     SendText(textSender, textPassword, textNumber, textMessage)
-    print "SmartScan complete. Exiting.."
+    print "Scan complete. Exiting.."
 
-""" Get the current torrents and cycle through to check all are finished. If all torrents 
-    are finished downloading, continue on to scanning and moving the files 
-"""
 def TorrentHandle():
+    """ 
+    Get the current torrents and cycle through to check all are finished. If all torrents 
+    are finished downloading, continue on to scanning and moving the files 
+    """
     tc = transmissionrpc.Client('localhost', port=9091, user=trpcUser, password=trpcPassword)
     currentTorrents = tc.get_torrents()
     for torrent in currentTorrents:
@@ -113,9 +97,7 @@ def TorrentHandle():
             tc.remove_torrent(torrent.hashString)
             print "Torrent removed"
         else:
-            print bcolor.RED + "Error: " + torrent.status + bcolor.ENDC
-            SendText(textSender, textPassword, textNumber, torrent.status)
-            pickle.dump( torrent.status, open( "tor.p", "wb" ) )
+            print color.RED + "Error: " + torrent.status + color.ENDC
             sys.exit(0)
 
 if __name__ == "__main__":
